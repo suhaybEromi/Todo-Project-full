cde9357fe403add4bd143edfea078d3ecbe1dc61
suhayb <suhayb@gmail.com>
2024-02-10 15:15:42 +0300

    Create File .gitignore

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("test"));
+app.get("/", (req, res) => res.send("Hello"));
 app.listen(3000, console.log("Running"));
