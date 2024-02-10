639b72b5b459cf1b697e27f0f2386059daed347c
suhayb <suhayb@gmail.com>
2024-02-10 17:30:52 +0300

    Connection DB

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+const db = require("./db");
 
-app.get("/", (req, res) => res.send("Hello"));
-app.listen(3000, console.log("Running"));
+app.get("/", async (req, res) => {
+  const data = await db.select("*").from("user");
+  console.log(data);
+  res.send(data);
+});
+app.listen(3000, () => console.log("Running"));
